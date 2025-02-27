8209948634b254f736cbb8b7d4da836d7bf119ac
testgit config --global user.name test <tranquangloc2004@gmail.com>
2025-02-27 10:56:53 +0700

    second

--- JdbcAppDemo/src/main/java/com/tql/jdbcappdemo/JdbcAppDemo.java
+++ JdbcAppDemo/src/main/java/com/tql/jdbcappdemo/JdbcAppDemo.java
@@ -5,6 +5,7 @@ package com.tql.jdbcappdemo;
 
 import com.tql.pojo.Choice;
 import com.tql.services.QuestionServices;
+import com.tql.services.StatsServices;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
@@ -18,30 +19,34 @@ import java.util.logging.Logger;
 public class JdbcAppDemo {
 
     public static void main(String[] args) throws SQLException {
-        Scanner sc= new Scanner(System.in);
-        QuestionServices s = new QuestionServices();
-        s.getQuestions(2).forEach(q -> {
-            
-            try {
-                System.err.println(q);
-
-                List<Choice> choices = s.getChoices(q.getId());
-                for(int i=0;i<choices.size();i++)
-                
-                    System.err.printf("%d. %s\n", i+1 ,choices.get(i));
-                    
-                System.err.print("Your Answer:  ");
-                int idx =  sc.nextByte();
-                if(choices.get(idx).isCorrect() == true )
-                    System.err.println("EXACTLY");
-                else
-                    System.out.println("WRONG");
-            } catch (SQLException ex) {
-                Logger.getLogger(JdbcAppDemo.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            
-
-        });
+        StatsServices sr= new StatsServices();
+        sr.countQuestionsByCate("").forEach(s -> System.err.printf("%d -%s: %d\n", s[0],s[1],s[2]));
+        
+        
+//        Scanner sc= new Scanner(System.in);
+//        QuestionServices s = new QuestionServices();
+//        s.getQuestions(2).forEach(q -> {
+//            
+//            try {
+//                System.err.println(q);
+//
+//                List<Choice> choices = s.getChoices(q.getId());
+//                for(int i=0;i<choices.size();i++)
+//                
+//                    System.err.printf("%d. %s\n", i+1 ,choices.get(i));
+//                    
+//                System.err.print("Your Answer:  ");
+//                int idx =  sc.nextByte();
+//                if(choices.get(idx).isCorrect() == true )
+//                    System.err.println("EXACTLY");
+//                else
+//                    System.out.println("WRONG");
+//            } catch (SQLException ex) {
+//                Logger.getLogger(JdbcAppDemo.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//
+//            
+//
+//        });
     }
 }
